77040652c1310e0039bbfbbfefc052fdacba8dd9
Sergi Juanati <wint3rmute@gmail.com>
2020-11-22 19:18:37 +0100

    Using SafeMath

--- contracts/StakingPool.sol
+++ contracts/StakingPool.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.5;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+
 /// @notice This is the Ethereum 2.0 deposit contract inter face.
 interface IDepositContract {
     /// @notice Submit a Phase 0 DepositData object.
@@ -19,14 +22,15 @@ interface IDepositContract {
 
 
 contract StakingPool {
+    using SafeMath for uint256;
     address payable public owner;
-    mapping (address => uint256) public balances;
-    mapping (address => bool) public changeClaimed;
     bool public finalized;
     uint256 public end;
     uint256 constant stake = 32 ether;
     uint256 public totalInvested;
     uint256 public totalChange; // replace Change by Extra
+    mapping (address => uint256) public balances;
+    mapping (address => bool) public changeClaimed;
     mapping (bytes => bool) public pubkeysUsed; // pubkeys for Validators
     IDepositContract public depositContract = IDepositContract(0x00000000219ab540356cBB839Cbe05303d7705Fa);
     
@@ -36,7 +40,7 @@ contract StakingPool {
     
     constructor() public {
         owner = msg.sender;
-        end = block.timestamp + 7 days; // TODO: instead, make function to start period
+        end = block.timestamp.add(7 days); // TODO: instead, make function to start period
     }
     
     function invest() external payable {
@@ -44,8 +48,9 @@ contract StakingPool {
         if (balances[msg.sender] == 0) {
             emit NewInvestor(msg.sender);
         }
-        uint256 fee = msg.value * 1 / 100;
-        uint256 amountInvested = msg.value - fee;
+        //uint256 fee = msg.value * 1 / 100;
+        uint256 fee = msg.value.mul(15).div(1000);
+        uint256 amountInvested = msg.value.sub(fee);
         owner.transfer(fee);
         balances[msg.sender] += amountInvested; // TODO: safemath
     }
@@ -57,7 +62,8 @@ contract StakingPool {
         require(finalized == false, 'already finalized');
         finalized = true;
         totalInvested = address(this).balance;
-        totalChange = address(this).balance % stake; // to be distributed to investors
+        //totalChange = address(this).balance % stake;
+        totalChange = address(this).balance.mod(stake);
     }
     
     //@dev: allow each investor to claim his/her change
@@ -67,7 +73,8 @@ contract StakingPool {
         require(balances[msg.sender] > 0, 'not an investor');
         require(changeClaimed[msg.sender] == false, 'change already claimed');
         changeClaimed[msg.sender] = true;
-        uint256 amount = totalChange * balances[msg.sender] / totalInvested; // amount of change to be distributed to investors
+        // Amount of change to be distributed to investors
+        uint256 amount = totalChange.mul(balances[msg.sender].div(totalInvested));
         msg.sender.transfer(amount);
     }
     
